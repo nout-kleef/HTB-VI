0311016411caf4a66954515e97fdb0c7b6a5c6f3
slaifan <fassouleh200@gmail.com>
2020-02-29 17:32:13 +0000

    Implement trader class

--- optiver/trader.py
+++ optiver/trader.py
@@ -7,11 +7,37 @@ class Trader():
     INSTRUMENTS = ["SP-FUTURE", "ESX-FUTURE"]
 
     def __init__(self, conversionLine, baseLine, spanB, source):
-        self.marketHistory = []
+        self.marketHistory = [] ## list of histograms for both markets
         for instr in Trader.INSTRUMENTS:
             self.marketHistory.add(Histogram(spanB, instr))
-        # start listening
-        self.listen(source)
+        
+        self.listen(source)# start listening
+        self.conversionLine = conversionLine
+        self.baseLine = baseLine
+        self.spanB = spanB
+        
 
-    def listen(self, source):
-        pass
+    def listen(self, source): ## parses if source == CSV and listens if live
+        if source == "CSV":
+            with open(r'C:\Users\F.K\Desktop\HTB\HTB-VI\market_data.csv') as market_data:
+                for line in market_data:
+                    comps = message.split(",")
+                    Timestamp = comps[0]   ## as string
+                    instrument = comps[1]  ## as string
+                    bid_price = float(comps[2])  
+                    bid_volume = int(comps[3]) 
+                    ask_price = float(comps[4])
+                    ask_volume = int(comps[5])
+                    market_state = MarketState(bid_price, ask_price, bid_volume, ask_volume)
+                    self.update_market(market_state, instrument)
+                    self.tradeCSV() 
+
+        else:
+            pass    
+
+    def update_market(self, market_state, instrument):
+        index = self.marketHistory.index(instrument)
+        self.marketHistory[index].marketUpdate(market_state)
+
+    def tradeCSV(self):
+        pass ## add algorithm here and check the constructor for our data
